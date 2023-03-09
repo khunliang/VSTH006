7623d17e830bb9e47960702b93f5c709cbee78f7
khunliang <94458459+khunliang@users.noreply.github.com>
2023-03-09 19:07:52 +0700

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+let content2 = document.getElementById('content-2');
+let textHtml = '<b>怎么能不呢？</b>';
+textHtml += '<i>对 不 起，我 的 中 文 不 好</i>'；
+content2.innerHTML = textHtml;
+
+
+let discountButton = document.getElementById('discount-button')；
+let message = document.getElementById('message')；
+
+function showMessage(){
+    message.innerHTML = '来迟了'；
+}
+
+discountButton.addEventListener()
