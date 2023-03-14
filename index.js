fb67aa34b67c036e0ed0687252c02a3fd141feba
khunliang <94458459+khunliang@users.noreply.github.com>
2023-03-14 18:54:56 +0700

    change

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let content2 = document.getElementById('content-2');
 let textHtml = '<b>怎么能不呢？</b>';
-textHtml += '<i>对 不 起，我 的 中 文 不 好</i>'；
+textHtml += '<i>对 不 起，我 的 中 文 不 好</i>';
 content2.innerHTML = textHtml;
 
 
-let discountButton = document.getElementById('discount-button')；
-let message = document.getElementById('message')；
+let discountButton = document.getElementById('discount-button');
+let message = document.getElementById('message');
 
 function showMessage(){
-    message.innerHTML = '你 们 来 迟 了'；
+    message.innerHTML = '你 们 来 迟 了';
 }
 
-discountButton.addEventListener('click',showMessage)；
+discountButton.addEventListener('click',showMessage);
